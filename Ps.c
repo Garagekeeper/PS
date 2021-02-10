ded851c7f0a00d8c086294041464866be3e88ac1
Garagekeeper <67981590+Garagekeeper@users.noreply.github.com>
2021-02-10 11:13:56 +0900

    Update Ps.c

--- Ps.c
+++ Ps.c
@@ -1,23 +1,67 @@
+/*
+문제 1181
+알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+
+길이가 짧은 것부터
+길이가 같으면 사전 순으로
+
+첫째 줄에 단어의 개수 N이 주어진다. (1 ≤ N ≤ 20,000) 둘째 줄부터 N개의 줄에 걸쳐 알파벳 소문자로 이루어진 단어가 한 줄에 하나씩 주어진다. 주어지는 문자열의 길이는 50을 넘지 않는다.
+
+조건에 따라 정렬하여 단어들을 출력한다. 단, 같은 단어가 여러 번 입력된 경우에는 한 번씩만 출력한다.
+*/
 #include <stdio.h>
-char* str[][51];
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct str_info{
+    char str[51];
+    int length;
+}str_info;
+
+struct str_info info_arr[200000];
+
+int compare(const void* a,const void* b){
+    struct str_info *s1 = (str_info *)a;
+    struct str_info *s2 = (str_info *)b;
+
+    if(s1->length > s2->length) return 1;
+    else if(s1->length < s2->length) return -1;
+    return 0;
+}
 
 void sort(int n){
-    int i=0;
-    while(i<n){
-        printf("%s\n",str[i]);
-        i++;
+    qsort(info_arr,n,sizeof(str_info),compare); //1차 정렬(길이 오름차순)
+
+    for(int i=0;i<n-1;i++){
+        
     }
 
+    for(int i=0;i<n;i++){
+        printf("%s\n",info_arr[i].str);
+    }
+}
+
+int check(char* str,int n){
+    for(int i=0;i<n;i++){
+        if(strcmp(info_arr[i].str,str)==0 && info_arr[i].length == strlen(str))
+            return 1;
+    }   
+
+    return 0;
 }
 
 int main(){
     
     int n;
-
+    char temp[51];
     scanf("%d",&n);
-    fflush(stdin);
     for(int i=0;i<n;i++){
-        scanf("%s",str[i]);
+        scanf("%s",temp);
+        if(check(temp,i))
+            continue;
+        strcpy(info_arr[i].str,temp);
+        info_arr[i].length = strlen(temp); 
+
     }
     sort(n);
 
