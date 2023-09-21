0a972b2eaba15e528686dd66a5eeafcab672db87
Supply_depot <hj06869@naver.com>
2023-09-21 23:09:45 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2540 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+int abc[51][51][51];
+
+int w(int a, int b, int c) {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return abc[0][0][0];
+  }
+
+  if (a > 20 || b > 20 || c > 20) {
+    return abc[20][20][20];
+  }
+
+  return abc[a][b][c];
+}
+
+int main() {
+  int a, b, c;
+
+  for (int i = 0; i < 51; i++) {
+    for (int j = 0; j < 51; j++) {
+      for (int k = 0; k < 51; k++) {
+        if (i == 0 || j == 0 || k == 0) {
+          abc[i][j][k] = 1;
+        } else if (i < j && j < k) {
+          abc[i][j][k] =
+              abc[i][j][k - 1] + abc[i][j - 1][k - 1] - abc[i][j - 1][k];
+        } else {
+          abc[i][j][k] = abc[i - 1][j][k] + abc[i - 1][j - 1][k] +
+                         abc[i - 1][j][k - 1] - abc[i - 1][j - 1][k - 1];
+        }
+      }
+    }
+  }
+
+  while (true) {
+    scanf("%d %d %d", &a, &b, &c);
+    if (a == -1 && b == -1 && c == -1) break;
+    printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+  }
+}
