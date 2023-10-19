cb819510b7b799e709cd4b7681e62e369b5a056d
Supply_depot <hj06869@naver.com>
2023-10-20 02:41:49 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 112 ms, Memory: 10896 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define MAX 100001
+
+int n, k, r;
+bool visited[MAX];
+int result[MAX];
+int cnt = 0;
+std::vector<int> graph[MAX];
+
+void dfs(int r) {
+  if (visited[r] == 1) return;
+  visited[r] = 1;
+  cnt++;
+  result[r] = cnt;
+  for (int i = 0; i < graph[r].size(); i++) {
+    dfs(graph[r][i]);
+  }
+}
+
+int main() {
+  scanf("%d %d %d", &n, &k, &r);
+  while (k--) {
+    int u, v;
+    scanf("%d %d", &u, &v);
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+  }
+
+  for (int i = 1; i <= n; i++) {
+    sort(graph[i].begin(), graph[i].end());
+  }
+  dfs(r);
+  for (int i = 1; i <= n; i++) {
+    printf("%d\n", result[i]);
+  }
+}
