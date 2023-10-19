a49370f819d0024fc10ea1ae1b22e0434e223370
Supply_depot <hj06869@naver.com>
2023-10-20 05:07:38 +0900

    [Silver II] Title: 최대 힙, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.cc
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
-#define MAX 11
 
 int n;
-int cnt, res;
 int main() {
   std::priority_queue<int> pq;
+    
   scanf("%d", &n);
+    
   while (n--) {
     int temp;
     scanf("%d\n", &temp);
+      
     if (temp == 0) {
       if (pq.size() == 0) {
         printf("0\n");
