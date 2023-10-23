7bc1c1c6ccb9f1d0d7fb2e03af76c2094dca6131
Supply_depot <hj06869@naver.com>
2023-10-24 02:47:41 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 132 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -16,19 +16,10 @@ int main() {
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= n; j++) {
       cin >> arr[i][j];
-    }
-  }
-
-  for (int i = 1; i <= n; i++) {
-    sum[i][1] = sum[i - 1][1] + arr[i][1];
-    sum[1][i] = sum[1][i - 1] + arr[1][i];
-  }
-
-  for (int i = 2; i <= n; i++) {
-    for (int j = 2; j <= n; j++) {
       sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + arr[i][j];
     }
   }
+
 
   while (m--) {
     cin >> x1 >> y1 >> x2 >> y2;
