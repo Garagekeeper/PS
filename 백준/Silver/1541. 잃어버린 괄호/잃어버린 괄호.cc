7cbde36bd150390f4c9e5d79cf0c0b89a34d5f7a
Supply_depot <hj06869@naver.com>
2023-10-29 01:14:06 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,10 +1,5 @@
 #include <bits/stdc++.h>
-#define MAX 1001
 using namespace std;
-
-stack<int> st;
-stack<int> st2;
-vector<int> vec;
 
 int main() {
   ios::sync_with_stdio(false);
@@ -14,42 +9,25 @@ int main() {
   string s;
   string s2;
   cin >> s;
-  s += 'e';
   int res;
-  
-  for (int i = 0; i < s.size(); i++) {
-    if ('0' <= s[i] && s[i] <= '9') {
-      s2 += s[i];
-    } else {
-      if (s[i] == '-') {
-        st2.push(0);
-      } else if (s[i] == '+') {
-        st2.push(1);
-      }
-      st.push(stoi(s2));
-      s2 = "";
-    }
-  }
 
-  bool flag = true;
-  res       = st.top();
-  st.pop();
-  while (!st.empty()) {
-    if (st2.top() == 0) {
-      if (flag) {
-        flag = false;
+  bool flag = false;
+
+  for (int i = 0; i <= s.size(); i++) {
+    if (s[i] == '-' || s[i] == '+' || s[i] == '\0') {
+      if (flag)
+        res -= stoi(s2);
+      else
+        res += stoi(s2);
+
+      s2 = "";
+
+      if (s[i] == '-') {
+        flag = true;
+        continue;
       }
-      vec.push_back(-1 * res);
-      res = st.top();
-    } else {
-      flag = true;
-      res += st.top();
     }
-    st.pop();
-    st2.pop();
-  }
-  for (auto a : vec) {
-    res += a;
+    s2 += s[i];
   }
   cout << res;
 }
