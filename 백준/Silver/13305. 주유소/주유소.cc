0e9e38f7c790035eb6c30bdf09976f6fc5d941e4
Supply_depot <hj06869@naver.com>
2023-10-29 23:15:49 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 3192 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define MAX 100001
+using namespace std;
+
+int dist[MAX];
+int city[MAX];
+int city_minimun[MAX];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  long long res  = 0;
+  cin >> n;
+
+  for (int i = 1; i < n; i++) {
+    cin >> dist[i];
+  }
+
+  for (int i = 1; i <= n; i++) {
+    cin >> city[i];
+  }
+
+  city_minimun[1] = city[1];
+  for (int i = 2; i <= n; i++) {
+    city_minimun[i] = min(city[i], city[i - 1]);
+  }
+
+  for (int i = 1; i < n; i++) {
+    res += city_minimun[i] * dist[i];
+  }
+
+  cout << res;
+}
