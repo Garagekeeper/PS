aedf63bc46d02814b7df12fcee78883889c98296
Supply_depot <hj06869@naver.com>
2023-10-30 22:41:38 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 #define MAX 46341
 using namespace std;
-long long arr[MAX];
 int a, b, c;
 
 int DivideAndConquer(int _a, int _b, int _c) {
   long long res = 1;
   if (_b == 1) return _a % _c;
-  if (_b <= MAX && arr[_b]) return arr[_b];
   res = DivideAndConquer(_a, _b / 2, _c);
   if (_b % 2 == 0) {
     res = (res * res) % _c;
@@ -23,10 +21,6 @@ int main() {
   cin.tie(NULL);
   cout.tie(NULL);
     
-  arr[0] = 1;
   cin >> a >> b >> c;
-  for (int i = 1; i <= (int)sqrt(b); i++) {
-    arr[i] = (arr[i - 1] * a) % c;
-  }
   cout << DivideAndConquer(a, b, c);
 }
