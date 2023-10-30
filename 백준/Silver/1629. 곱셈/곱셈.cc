a21c4f56c6bd576c108faaca3a0e56ee26c7f2b1
Supply_depot <hj06869@naver.com>
2023-10-30 22:41:13 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2396 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,36 +1,32 @@
 #include <bits/stdc++.h>
+#define MAX 46341
 using namespace std;
-unsigned long long arr[46341];
+long long arr[MAX];
+int a, b, c;
 
-long long a, b, c;
-long long DivideAndConquer(int a, int b, int c) {
-  unsigned long long res = 1;
-  if (b == 1) return a % c;
-  res = DivideAndConquer(a, b / 2, c);
-  if (b % 2 == 0) {
-    res = (res * res) % c;
+int DivideAndConquer(int _a, int _b, int _c) {
+  long long res = 1;
+  if (_b == 1) return _a % _c;
+  if (_b <= MAX && arr[_b]) return arr[_b];
+  res = DivideAndConquer(_a, _b / 2, _c);
+  if (_b % 2 == 0) {
+    res = (res * res) % _c;
   } else {
-    res = ((res * res) %c * a) % c;
+    res = ((res * res) % _c * _a) % _c;
   }
 
-  return res % c;
+  return res;
 }
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
+    
   arr[0] = 1;
   cin >> a >> b >> c;
-  for (int i = 1; i < (int)sqrt(b); i++) {
+  for (int i = 1; i <= (int)sqrt(b); i++) {
     arr[i] = (arr[i - 1] * a) % c;
   }
-
   cout << DivideAndConquer(a, b, c);
 }
-
-// 55 -50 40 -30 20
-// 1 -2 -3 4 -5
-// 1 1 2 3 5 8 13
-// 7 3 3 4
-// 2147483646 2147483646 2147483647
