db6c18644e8b2dc37a4ef22ce8bea434627885e0
Supply_depot <hj06869@naver.com>
2023-12-18 00:30:36 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int weight[101];
-int dp[MAX];
+int dp[MAX] = {1, 0};
 int n, k;
 
 int main() {
@@ -13,21 +13,13 @@ int main() {
 
   cin >> n >> k;
 
+  // i번째 추까지 사용해서 만들 수 있는 j의 개수
   for (int i = 0; i < n; i++) {
-    int temp;
-    cin >> temp;
-    if (temp > MAX - 1) {
-      n--; i--;
-      continue;
-    }
-    weight[i] = temp;
-  }
-  //i번째 추까지 사용해서 만들 수 있는 j의 개수
-  for (int i = 0; i < n; i++) {
-    for (int j = 1; j <= k; j++) {
-      if (j == weight[i]) dp[j]++;
-      dp[j] += j - weight[i] > 0 ? dp[j - weight[i]] : 0;
+    int weight;
+    cin >> weight;
+    for (int j = weight; j <= k; j++) {
+      dp[j] += dp[j - weight];
     }
   }
-  cout << dp[k];
+  printf("%d", dp[k]);
 }
