e81cd930d07fd701cd5207fa9f97e470cf29121f
Supply_depot <hj06869@naver.com>
2024-03-04 01:06:07 +0900

    [Platinum V] Title: 히스토그램, Time: 8 ms, Memory: 2940 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.cc
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -1,29 +1,29 @@
-#include<iostream>
-#include<algorithm>
-#include<stack>
+#include <algorithm>
+#include <iostream>
+#include <stack>
+
+#define MAX 100002
 using namespace std;
 
-int N, ans, h[100002];
+int ans, N, h[MAX];
 stack<int> s;
 
-int main() 
-{
-	cin >> N;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  cin >> N;
 
-	for (int i = 1; i <= N; i++) cin >> h[i];
+  for (int i = 1; i <= N; i++) cin >> h[i];
 
-	s.push(0);
-	for (int i = 1; i <= N + 1; i++)
-	{
-
-		while (!s.empty() && h[s.top()] > h[i])
-		{
-			int check = s.top();
-			s.pop();
-			ans = max(ans, h[check]*(i - s.top() - 1));
-		}
-		s.push(i);
-	}
-	cout << ans;
-	
+  s.push(0);
+  for (int i = 1; i <= N + 1; i++) {
+    while (!s.empty() && h[s.top()] > h[i]) {
+      int check = s.top();
+      s.pop();
+      ans = max(ans, h[check] * (i - s.top() - 1));
+    }
+    s.push(i);
+  }
+  cout << ans;
 }
