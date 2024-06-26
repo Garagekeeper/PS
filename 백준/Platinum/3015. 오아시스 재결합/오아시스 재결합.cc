a7cac6663169016182d00a1e520d96767b8745f7
Supply_depot <hj06869@naver.com>
2024-06-26 21:23:15 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 72 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -2,25 +2,38 @@
 #include <stack>
 using namespace std;
 
-struct e {int h, cnt;};
+struct e { int h, cnt; };
 
-int main(){
-  ios::sync_with_stdio(0);
-  cin.tie(0);cout.tie(0);
+int N;
+stack<e> st;
 
-  int n; cin >> n;
-  stack<e> S;
-  long long ans=0;
-  while (n--) {
-    int x; cin >> x;
-    int cnt = 1;
-    while(!S.empty() && S.top().h <= x) {
-      ans += S.top().cnt; // t보다 크거나 같은 사람(k)
-      if (S.top().h == x) cnt += S.top().cnt;
-      S.pop();
-    }
-    if(!S.empty()) ans++; // x앞에 x보다 큰 사람
-    S.push({x, cnt});
-  }
-  cout << ans;
+long long ans = 0;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		int cnt = 1;
+		cin >> temp;
+		while (!st.empty() && st.top().h <= temp)
+		{
+			ans += st.top().cnt;
+			if (st.top().h == temp) cnt += st.top().cnt;
+			st.pop();
+		}
+
+		if (!st.empty()) ans++;
+		st.push({ temp, cnt });
+	}
+
+	cout << ans << "\n";
+
+
+	return 0;
 }
