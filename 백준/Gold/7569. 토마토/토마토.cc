2f7475dbc70fb5a7d74a44375397f5cfc26e5bfb
Supply_depot <hj06869@naver.com>
2024-06-29 22:41:30 +0900

    [Gold V] Title: 토마토, Time: 232 ms, Memory: 8360 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <iomanip>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+#include <tuple>
+
+using namespace std;
+#define MAX 100
+int M, N, H;
+int curx, cury;
+int destx, desty;
+
+int map[MAX][MAX][MAX] = { 0, };
+bool visited[MAX][MAX][MAX] = { 0, };
+queue<tuple<int, int, int>> q;
+
+void GetInput()
+{
+	cin >> M >> N >> H;
+
+	for (int i = 0; i < H; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			for (int k = 0; k < M; k++)
+			{
+				int temp;
+				cin >> temp;
+				map[i][j][k] = temp;
+				if (temp == 1)
+				{
+					visited[i][j][k] = true;
+					q.push(make_tuple(i, j, k));
+				}
+			}
+		}
+	}
+}
+
+bool CanGo(int z, int x, int y)
+{
+	if (x < 0 || x >= N) return false;
+	if (y < 0 || y >= M) return false;
+	if (z < 0 || z >= H) return false;
+	if (map[z][x][y] == -1) return false;
+	if (visited[z][x][y]) return false;
+	return true;
+}
+
+void BFS()
+{
+	int dx[6] = { 0, 1, 0,-1, 0, 0 };
+	int dy[6] = { 1, 0,-1, 0, 0, 0 };
+	int dz[6] = { 0, 0, 0, 0 ,1 ,-1 };
+
+	while (!q.empty())
+	{
+		int frtX = get<1>(q.front());
+		int frtY = get<2>(q.front());
+		int frtZ = get<0>(q.front());
+		q.pop();
+
+
+		for (int i = 0; i < 6; i++)
+		{
+			int nx = frtX + dx[i];
+			int ny = frtY + dy[i];
+			int nz = frtZ + dz[i];
+
+			if (CanGo(nz, nx, ny))
+			{
+				visited[nz][nx][ny] = true;
+				map[nz][nx][ny] = map[frtZ][frtX][frtY] + 1;
+				q.push(make_tuple(nz, nx, ny));
+			}
+		}
+	}
+
+}
+
+void PrintAns()
+{
+	int cnt = 0;
+	for (int i = 0; i < H; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			for (int k = 0; k < M; k++)
+			{
+				if (map[i][j][k] == 0)
+				{
+					printf("-1\n");
+					return;
+				}
+
+				cnt = max(cnt, map[i][j][k]);
+			}
+		}
+	}
+	printf("%d", cnt - 1);
+}
+
+int main()
+{
+
+	GetInput();
+	BFS();
+	PrintAns();
+
+	return 0;
+}
