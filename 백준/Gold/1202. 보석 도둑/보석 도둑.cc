71474e05cf25d0fce241daa05de30fb8be3f37e1
Supply_depot <hj06869@naver.com>
2025-05-07 02:12:33 +0900

    [Gold II] Title: 보석 도둑, Time: 152 ms, Memory: 8740 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,95 +1,52 @@
-#include <iostream>
-#include <queue>
-#include <algorithm>
-#include <set>
-using namespace std;
-
 /*
-    When true is returned 
-    it means the order is NOT correct and swapping of elements takes place.
-
-    if (a > b) return true or return a > b
-    -> a > b 인 상태가 swap이 필요한 상태. (오름차순)
+보석을 가치가 높은 순으로 정렬
+용량이 작은 가방부터 들어갈 수 있는 모든 보석을 최대 힙에 넣고 가장 가치가 높은 것을 채택. 이를 위해 보석과 가방의 무게와 허용 무게를 오름차순 정렬해야 함.
+가방 용량이 커질수록 들어갈 수 있는 보석은 많아진다. 그러나 여전히 가치가 높은 것을 채택(우선순위 큐)
 */
 
-struct Jewel
-{
-public:
-    Jewel(){}
-    Jewel(int m, int v) : mass(m), value(v) { }    
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
 
-    int mass;
-    int value;
-};
+int main(int argc, char* argv[]) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-struct Compare
-{
-    bool operator()(Jewel a, Jewel b) 
-    {
-        if (a.value < b.value)
-            return true;
-        else if (a.value == b.value)
-        {
-            if (a.mass > b.mass)
-            return true;
-        }
+    int N, K;
+    std::cin >> N >> K;
 
-        return false;
+    std::vector<std::pair<int, int>> gems(N);
+    for (int i = 0; i < N; i++) {
+        std::cin >> gems[i].first >> gems[i].second;
     }
-};
-
-
-int main()
-{
-    ios_base :: sync_with_stdio(false); 
-    cin.tie(NULL); 
-    cout.tie(NULL);
-
-    priority_queue<Jewel , vector<Jewel>, Compare> pq;
-    //priority_queue<Jewel , vector<Jewel>, greater<int>> pq;
-    multiset<int> sackVec;
     
-    int N,K;
-    int M,V;
-    int MAX;
-
-    cin >> N >> K;
-
-    while (N--)
-    {
-        cin >> M >> V;
-        pq.push(Jewel(M,V));
+    std::vector<int> bags(K);
+    for (int i = 0; i < K; i++) {
+        std::cin >> bags[i];
     }
 
-    while (K--)
-    {
-        int C;
-        cin >> C;
-        MAX = max(C,MAX);
-        sackVec.insert(C);
-    }
+    std::sort(gems.begin(), gems.end(), [](const auto& a, const auto& b) {
+        return a.first < b.first;
+    });
+    std::sort(bags.begin(), bags.end());
 
-    long long maxValue = 0;
-    //sort(sackVec.begin(), sackVec.end());
+    long long total_value = 0;
+    int j = 0;
+    std::priority_queue<int> pq;
+    for (int i = 0; i < K; i++) {
+        while (j < N && gems[j].first <= bags[i]) {
+            pq.push(gems[j].second);
+            j++;
+        }
 
-    while (!pq.empty())
-    {
-        Jewel top = pq.top();
-        pq.pop();
-        if (top.mass > MAX) continue;
-        if (sackVec.empty()) break;
-        for (int i = 0; i < sackVec.size(); i++)
-        {
-            auto it = sackVec.lower_bound(top.mass);
-            if (it != sackVec.end())
-            {
-                maxValue += top.value;
-                sackVec.erase(it);
-                break;
-            }
+        if (!pq.empty()) {
+            total_value += pq.top();
+            pq.pop();
         }
     }
 
-    cout << maxValue;
+    std::cout << total_value;
+
     return 0;
 }
