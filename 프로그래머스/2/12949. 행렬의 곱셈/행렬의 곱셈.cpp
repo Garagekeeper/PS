e6ec1d8db892a508808728c154528f9773476a17
Supply_depot <hj06869@naver.com>
2025-06-23 19:22:06 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.01 ms, Memory: 5.63 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -5,16 +5,41 @@ using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
     vector<vector<int>> answer;
-    //ab x bc 면 ac의 행렬 생성
+    //a*b x b*c 면 ac의 행렬 생성
+    /*
+    행렬곱 예시
+    {1 2} {1 2 3 } => {(1*1 + 2*4), (1*2 + 2*5), (1*3 + 2*6)} = {9 ,12, 15}
+    {3 4} {4 5 6 }    {(3*1 + 4*4), (3*2 + 4*5), (3*3 + 4*6)}   {19,26, 33}
+    */
     answer.resize(arr1.size(), vector<int>(arr2[0].size()));
     
-    for (int i=0; i < arr1.size(); i++)
+    int leftMatrixRowCnt = arr1.size();
+    // left col cnt == right row cnt
+    int leftMatrixColCnt = arr1[0].size();
+    int rightMatrixColCnt = arr2[0].size();
+    
+    for (int i=0; i < leftMatrixRowCnt; i++)
     {
-        for (int j=0; j < arr2[0].size(); j++)
+        for (int j=0; j < rightMatrixColCnt; j++)
         {
-            for (int k=0; k < arr1[0].size(); k++)
+            for (int k=0; k < leftMatrixColCnt; k++)
                answer[i][j] += arr1[i][k] * arr2[k][j];
         }
     }
     return answer;
 }
+
+
+
+
+
+
+
+
+
+
+    /*
+    행렬곱 예시
+    {1 2} {1 2 3 } => {(1*1 + 2*4), (1*2 + 2*5), (1*3 + 2*6)} = {9 ,12, 15}
+    {3 4} {4 5 6 }    {(3*1 + 4*4), (3*2 + 4*5), (3*3 + 4*6)}   {19,26, 33}
+    */
