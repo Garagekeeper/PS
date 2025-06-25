7641256188eb8c0962fd37e5ede441c0f3e552cb
Supply_depot <hj06869@naver.com>
2025-06-25 13:42:06 +0900

    [level 2] Title: n^2 배열 자르기, Time: 40.07 ms, Memory: 22.2 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -6,15 +6,18 @@ using namespace std;
 vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
     
+    // i의 값을 2차원 배열의 인덱스들로 바꿔서 풀이
     for (auto i = left; i<=right; i++)
     {
         int row = i % n;
         int col = i / n;
         
+        // Lower Triangle(x==y 포함)
         if (row >= col)
         {
             answer.push_back(row + 1);
         }
+        // Upper Triangle
         else if (row < col)
         {
             answer.push_back(col + 1);
