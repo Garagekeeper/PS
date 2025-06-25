5ba1882a45b8b75d6f96dba22446bdac3cbb8187
Supply_depot <hj06869@naver.com>
2025-06-25 22:33:31 +0900

    Update 괄호 회전하기.cpp

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -1,46 +1,49 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <map>
 
 using namespace std;
 
+bool validation(const string& s , int startIndex)
+{
+    map <char, char> match_pair = {
+        {')','('},
+        {'}','{'},
+        {']','['},
+    };
+    
+    stack<char> leftOpen;
+    for (int j=0; j < s.size(); j++)
+    {
+        // 왼쪽으로 j칸 회전
+        int index = (startIndex + j) % s.size();
+        char c = s[index];
+        
+        if (c == '(' || c == '[' || c == '{')
+        {
+            leftOpen.push(c);
+        }
+        else 
+        {
+            if (leftOpen.empty()) return false;
+            if (leftOpen.top() != match_pair[c]) return false;
+            
+            leftOpen.pop();
+        }
+    }
+    
+    return leftOpen.empty();
+}
+
 int solution(string s) {
-    int answer = -1;
+    int answer = 0;
     stack<char> leftOpen;
     
     for (int i = 0; i < s.size(); i++)
     {
-        int pushcnt = 0;
-        for (int j=0; j < s.size(); j++)
-        {
-            int index = (i + j) % s.size();
-            if (s[index] == '(' || s[index] == '{' || s[index] == '[')
-            {
-                leftOpen.push(s[index]);   
-                pushcnt++;
-            }
-            else 
-            {
-                if (leftOpen.empty())  continue;
-                
-                if (s[index] == ')')
-                {
-                    if (leftOpen.top() == '(') leftOpen.pop();
-                }
-                else if (s[index] == '}')
-                {
-                if (leftOpen.top() == '{') leftOpen.pop();
-                }
-                else if (s[index] == ']')
-                {
-                if (leftOpen.top() == '[') leftOpen.pop();
-                }
-
-            }
-        }
-        if (leftOpen.empty() && pushcnt!=0) answer++;
-        while (!leftOpen.empty()) leftOpen.pop();
+       if (validation(s, i)) answer++;
     }
-    answer++;
+    
     return answer;
 }
