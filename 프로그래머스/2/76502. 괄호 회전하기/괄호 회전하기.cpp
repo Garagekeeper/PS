23eeb4adcb13a2fa11158ff1be7c962a746c86a1
Supply_depot <hj06869@naver.com>
2025-06-25 22:03:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 12.73 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = -1;
+    stack<char> leftOpen;
+    
+    for (int i = 0; i < s.size(); i++)
+    {
+        int pushcnt = 0;
+        int popcnt = 0;
+        for (int j=0; j < s.size(); j++)
+        {
+            int index = (i + j) % s.size();
+            if (s[index] == '(' || s[index] == '{' || s[index] == '[')
+            {
+                leftOpen.push(s[index]);   
+                pushcnt++;
+            }
+            else 
+            {
+                if (leftOpen.empty())  continue;
+                
+                if (s[index] == ')')
+                {
+                    if (leftOpen.top() == '(') leftOpen.pop();
+                }
+                else if (s[index] == '}')
+                {
+                if (leftOpen.top() == '{') leftOpen.pop();
+                }
+                else if (s[index] == ']')
+                {
+                if (leftOpen.top() == '[') leftOpen.pop();
+                }
+                
+                popcnt++;
+            }
+        }
+        if (leftOpen.empty() && (pushcnt == popcnt) && pushcnt != 0) answer++;
+        while (!leftOpen.empty()) leftOpen.pop();
+    }
+    answer++;
+    return answer;
+}
