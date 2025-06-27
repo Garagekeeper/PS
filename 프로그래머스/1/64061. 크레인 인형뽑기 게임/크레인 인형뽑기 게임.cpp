2368e966b4ce382e9367520adde4ca9d7de807ff
Supply_depot <hj06869@naver.com>
2025-06-28 01:27:04 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 2.53 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -0,0 +1,59 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+bool isSameElement(stack<int> st, const int& e)
+{
+    if (st.empty()) return false;
+    
+    if (st.top() == e) return true;
+    
+    return false;
+}
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    stack<int> crane;
+    int answer = 0;
+    
+    for (auto e : moves)
+    {
+        for (int i = 0; i < board.size(); i++)
+        {
+            int target = board[i][e-1];
+            if (target != 0)
+            {
+                if (isSameElement(crane, target))
+                {
+                    crane.pop();
+                    answer++;
+                    answer++;
+                }
+                else 
+                {
+                    crane.push(target);
+                }
+                board[i][e-1] = 0;
+                break;
+            }
+        }
+        
+        stack<int> temp;
+        while (!crane.empty())
+        {
+            temp.push(crane.top());
+            crane.pop();
+        }
+        
+        while (!temp.empty())
+        {
+            
+            crane.push(temp.top());
+            temp.pop();
+        }
+    }
+    return answer;
+}
