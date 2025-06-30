a02db3bdc7f8e06fb718898c2e0c21d79beea0d0
Supply_depot <hj06869@naver.com>
2025-07-01 08:57:27 +0900

    [level 2] Title: 주식가격, Time: 29.84 ms, Memory: 26.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=cpp#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 26.7 MB, 시간: 25.81 ms
+메모리: 26.9 MB, 시간: 29.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 01일 07:19:37
+2025년 07월 01일 08:57:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -7,33 +7,25 @@ using namespace std;
 vector<int> solution(vector<int> prices) 
 {
     vector<int> answer(prices.size(), 0);
-    stack<pair<int, int>> priceStack;
-    int time = 0;
+    stack<int> priceStack;
     // 각 원소는 한번씩 push 와 pop을 한다.
     // 즉 N개 원소는 각각 N번의 push N번의 pop을 한다.
     // 2N -> O(N)
-    for (auto it = prices.begin(); it != prices.end(); ++it)
+    for (int i=0; i<prices.size(); i++)
     {
-        time++;
-        
-        // top이 현재 가격보다 작거나 같을때 까지 pop
-        while (!priceStack.empty() && priceStack.top().first > *it)
+        while(!priceStack.empty() && prices[priceStack.top()] > prices[i])
         {
-            int insertTime = priceStack.top().second;
+            answer[priceStack.top()] = i - priceStack.top();
             priceStack.pop();
-                    
-            //삽입된 시점에 가격을 유지한 시간 기록
-            answer[insertTime - 1] = time - insertTime;
         }
-        priceStack.push(make_pair(*it, time));
+        
+        priceStack.push(i);
     }
-
     
     while(!priceStack.empty())
     {
-        int insertTime = priceStack.top().second;
+        answer[priceStack.top()] = prices.size() - 1 - priceStack.top();
         priceStack.pop();
-        answer[insertTime - 1] = time - insertTime;
     }
     
     return answer;
