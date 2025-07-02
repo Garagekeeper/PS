6747e9d87c5cf19fb42262cae8e45df9d1c6b8d9
Supply_depot <hj06869@naver.com>
2025-07-03 00:34:29 +0900

    [level 2] Title: 오픈채팅방, Time: 98.83 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
 
 ### 성능 요약
 
-메모리: 45.1 MB, 시간: 152.67 ms
+메모리: 43.7 MB, 시간: 98.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 02일 23:33:49
+2025년 07월 03일 00:34:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -1,37 +1,46 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-
+#include <iostream>
 using namespace std;
 
 vector<string> solution(vector<string> record) {
-    unordered_map<string, string> userMap;
-    vector<pair<string, string>> actions;
-    
-    for (const string& line : record) {
-        size_t space1 = line.find(' ');
-        size_t space2 = line.find(' ', space1 + 1);
-        string cmd = line.substr(0, space1);
-        string uid = line.substr(space1 + 1, space2 - space1 - 1);
-
-        if (cmd == "Enter") {
-            string name = line.substr(space2 + 1);
-            userMap[uid] = name;
-            actions.push_back({uid, "님이 들어왔습니다."});
-        }
-        else if (cmd == "Leave") {
-            actions.push_back({uid, "님이 나갔습니다."});
-        }
-        else if (cmd == "Change") {
-            string name = line.substr(space2 + 1);
-            userMap[uid] = name;
-        }
-    }
-
     vector<string> answer;
-    for (auto& [uid, action] : actions) {
-        answer.push_back(userMap[uid] + action);
+    unordered_map<string , string> userInfo;
+    for (auto it = record.begin(); it != record.end(); ++it)
+    {
+        if ((*it)[0] == 'E' || (*it)[0] == 'C')
+        {
+            auto startPos = (*it).find(" ") + 1;
+            auto endPos = (*it).find(" ", startPos);
+            
+            string uid = (*it).substr(startPos, endPos - startPos);
+            string nickName = (*it).substr(endPos + 1);
+            userInfo[uid] = nickName;
+        }
     }
+    
+    for (auto it = record.begin(); it != record.end(); ++it)
+    {
+        string res = "";
+        if ((*it)[0] == 'E')
+        {
+            auto startPos = (*it).find(" ") + 1;
+            auto endPos = (*it).find(" ", startPos);
+            string uid = (*it).substr(startPos, endPos - startPos);
+            res += (userInfo[uid] + "님이 들어왔습니다.");
 
+        }
+        else if ((*it)[0] == 'L')
+        {
+            auto startPos = (*it).find(" ") + 1;
+            string uid = (*it).substr(startPos);
+            res += (userInfo[uid] + "님이 나갔습니다.");
+        }
+        else
+            continue;
+        answer.push_back(res);
+    }
+    
     return answer;
 }
