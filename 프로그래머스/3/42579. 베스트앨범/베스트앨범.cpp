a39ef26f9e968880cb12891eb016a7cf346a39e0
Supply_depot <hj06869@naver.com>
2025-07-04 07:24:35 +0900

    Update 베스트앨범.cpp

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -5,13 +5,7 @@
 #include <algorithm>
 using namespace std;
 
-
-bool cmp1(pair<string, int> a, pair<string, int> b)
-{
-    return a.second > b.second;
-}
-
-bool cmp2(pair<int, int> a, pair<int, int> b)
+bool cmp(pair<int, int> a, pair<int, int> b)
 {
     if (a.second == b.second)
         return a.first < b.first;
@@ -21,30 +15,41 @@ bool cmp2(pair<int, int> a, pair<int, int> b)
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
+    // 장르의 총 재생횟수를 담는 맵
     unordered_map<string, int> totalCount;
+    // 위 변수를 정렬하기 위한 벡터
     vector<pair<string, int>> totalCountVec;
+    // 각 장르의 노래목록을 저장하는 맵
     unordered_map<string, vector<pair<int, int>>> songListInGenres;
     
+    // 장르별 총 재생횟수
+    // 장르별 노래 목록 저장
     for (int i = 0 ; i < genres.size(); i++)
     {
         totalCount[genres[i]] += plays[i];
         songListInGenres[genres[i]].push_back(make_pair(i, plays[i]));
     }
     
-    for (auto it = totalCount.begin(); it != totalCount.end(); ++it)
-    {
-        totalCountVec.push_back(make_pair(it->first, it->second));
-    }
+    // 정렬을 위해 벡터에 삽입
+    totalCountVec = vector<pair<string,int>>(totalCount.begin(), totalCount.end());
     
-    sort(totalCountVec.begin(), totalCountVec.end(), cmp1);
+    // 정렬
+    sort(totalCountVec.begin(), totalCountVec.end(), [](pair<string, int> a, pair<string, int> b){
+        return a.second > b.second;
+    });
     
+    // 많이 재생된 장르부터 순회
     for (auto it = totalCountVec.begin(); it != totalCountVec.end() ; ++it)
     {
+        // 장르 추출 후 곡 리스트 정렬 (재생수기준 내림, 동일하면 낮은 고유번호 순)
         string genres = it->first;
-        sort(songListInGenres[genres].begin(), songListInGenres[genres].end() ,cmp2);
+        sort(songListInGenres[genres].begin(), songListInGenres[genres].end() ,cmp);
+        // 2개씩 뽑음
         for (int i=0; i<2; i++)
         {
+            // 1개 밖에 없는 경우 체크해서 종료
             if (i == songListInGenres[genres].size()) break;
+            // 정답 배열에 곡의 고유번호를 넣음
             answer.push_back(songListInGenres[genres][i].first);
         }
     }
