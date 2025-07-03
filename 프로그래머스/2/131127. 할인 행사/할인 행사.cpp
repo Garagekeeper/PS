6fd91cd928cb3f0dfe34beffbf60184a2db3f66f
Supply_depot <hj06869@naver.com>
2025-07-04 05:15:09 +0900

    [level 2] Title: 할인 행사, Time: 10.30 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -4,42 +4,34 @@
 
 using namespace std;
 
+bool matchCheck(vector<string>& want, vector<int>& number, vector<string>& discount,  unordered_map<string, int>& disMap)
+{
+            
+    for (int j=0; j < want.size(); j++)
+    {
+        if (number[j] != disMap[want[j]])
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
     unordered_map<string, int> disMap;
-    for (int i = 0; i < 10; i++)
+    
+    for (int i = 0; i < discount.size(); i++)
     {
+ 
+        if (i > 9)
+            disMap[discount[i-10]]--;
         disMap[discount[i]]++;
-    }
-    
-    bool match = true;
-    for (int i=0; i<want.size(); i++)
-    {
-        if (number[i] != disMap[want[i]])
-            {
-                match = false;
-                break;
-            }
-    }
-    
-    if (match) answer++;
-    
-    for (int i = 10; i<discount.size(); i++)
-    {
-        disMap[discount[i-10]]--;
-        disMap[discount[i]]++;
-        
-        bool match = true;
-        for (int j=0; j < want.size(); j++)
-        {
-            if (number[j] != disMap[want[j]])
-            {
-                match = false;
-                break;
-            }
-        }
-        if (match) answer++;
 
+        if (matchCheck(want, number, discount, disMap)) answer++;
+        
     }
+  
     return answer;
 }
