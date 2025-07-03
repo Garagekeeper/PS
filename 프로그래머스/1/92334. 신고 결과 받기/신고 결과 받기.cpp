fdbbce73d1aed9d9ba3509c126c6192d36655955
Supply_depot <hj06869@naver.com>
2025-07-04 08:12:15 +0900

    [level 1] Title: 신고 결과 받기, Time: 181.17 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -9,9 +9,12 @@ using namespace std;
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer;
     
+    // 받은 메일 개수 저장
     unordered_map<string, int> mailList;
+    // 유저가 누구한테 신고 당했는지
     unordered_map<string, unordered_set<string>> reportLog;
     
+    // 누가 누굴 신고했는지 기록
     for (auto& e : report)
     {
         stringstream ss(e);
@@ -23,18 +26,20 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
         reportLog[to].insert(from);
     }
     
-    for (auto& v : reportLog)
+    // K회 이상 신고 당하면 신고한 사람들에게 메일
+    for (auto& [to, from] : reportLog)
     {
-        if (v.second.size() >= k)
+        if (from.size() >= k)
         {
-            for (auto& from : v.second)
+            for (auto& id : from)
             {
-                mailList[from]++;
+                mailList[id]++;
             }
         }
     }
     
-    
+    // 신고를 하지 않은 유저는 안만들어짐.
+    // mailList[id]를 통해서 없으면 만들어지는 과정이 들어감
     for (int i=0; i<id_list.size(); i++)
     {
         string id = id_list[i];
