5402366114432e85f805caf57412b6752ca4f8fe
Supply_depot <hj06869@naver.com>
2025-07-05 08:12:33 +0900

    [level 3] Title: 길 찾기 게임, Time: 28.28 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
@@ -1,88 +1,120 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-class Node 
+struct Node
 {
-public:
-    int data;
-    int x;
-    int y;
-    Node* leftChild = NULL;
-    Node* rightChild = NULL;
-
-    Node(int _data, int _x, int _y, Node* _leftChild, Node* _rightChild)
-        :data(_data), x(_x), y(_y), leftChild(_leftChild), rightChild(_rightChild)
-    { }
+    Node(int x, int num) : _x(x), _num(num) , _left(nullptr), _right(nullptr){}
+    int _x;
+    int _num;
+    
+    Node* _left;
+    Node* _right;
 };
 
-void BST_InsertNode(Node* tree, Node* node)
+class BST
 {
-    if (node->x < tree->x) {
-        if (tree->leftChild == NULL) {
-            tree->leftChild = node;
-            return;
-        }
-        else
-            BST_InsertNode(tree->leftChild, node);
+    public:
+    BST() : _root(nullptr) {};
+    void insert(int x, int num);
+    void PreOrder(vector<int>& vec);
+    void PostOrder(vector<int>& vec);
+    
+    private:
+    Node* insertPrivate(Node* node, int x,  int num);
+    void PreOrder(Node* node, vector<int>& vec);
+    void PostOrder(Node* node, vector<int>& vec);
+    Node* _root;
+};
+
+void BST::insert(int x, int num)
+{
+    if (_root == nullptr) 
+        _root = new Node(x,num);
+    else 
+    {
+        _root = insertPrivate(_root, x, num);
     }
-    else if (node->x > tree->x) {
-        if (tree->rightChild == NULL) {
-            tree->rightChild = node;
-            return;
-        }
-        else
-            BST_InsertNode(tree->rightChild, node);
+    
+}
+
+Node* BST::insertPrivate(Node* node, int x, int num)
+{
+    if (node == nullptr)
+    {
+        Node* newNode = new Node(x,num);   
+        return newNode;
     }
+    
+    if (node->_x < x)
+    {
+        node->_right = insertPrivate(node->_right, x, num);
+    }
+    else if (node->_x > x)
+    {
+        node->_left = insertPrivate(node->_left, x, num);
+    }
+    
+    return node;
 }
 
-void BST_PreOrder(Node* tree, vector<int>& pre)
+void BST::PreOrder(vector<int>& vec)
 {
-    if (tree == NULL)
-        return;
-    pre.push_back(tree->data);
-    BST_PreOrder(tree->leftChild, pre);
-    BST_PreOrder(tree->rightChild, pre);
+    PreOrder(_root, vec);
 }
 
-void BST_PostOrder(Node* tree, vector<int>& pos)
+void BST::PostOrder(vector<int>& vec)
 {
-    if (tree == NULL)
-        return;
-    BST_PostOrder(tree->leftChild, pos);
-    BST_PostOrder(tree->rightChild, pos);
-    pos.push_back(tree->data);
+    PostOrder(_root, vec);
 }
 
-bool cmp(Node& a, Node& b)
+void BST::PreOrder(Node* node, vector<int>& vec)
 {
-    if (a.y == b.y)
-        return a.x < b.x;
-    return a.y > b.y;
+    if (node == nullptr) return;
+
+    vec.push_back(node->_num);
+    PreOrder(node->_left, vec);
+    PreOrder(node->_right, vec);
+}
+
+void BST::PostOrder(Node* node, vector<int>& vec)
+{
+    if (node == nullptr) return;
+    PostOrder(node->_left, vec);
+    PostOrder(node->_right, vec);
+    vec.push_back(node->_num);
+}
+
+bool cmp (vector<int> vec1, vector<int> vec2)
+{
+    return vec1[1] > vec2[1];
 }
 
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) 
 {
-    vector<vector<int>> answer;
+    vector<vector<int>> answer(2,vector<int>());
+    unordered_map<int, int> xtonum;
     
-    vector<Node> nodes;
-    for (int i = 0; i < nodeinfo.size(); ++i)
-        nodes.push_back(Node(i + 1, nodeinfo[i][0], nodeinfo[i][1], NULL, NULL));
-    sort(nodes.begin(), nodes.end(), cmp);
-
-    for (int i = 1; i < nodes.size(); ++i)
-        BST_InsertNode(&nodes[0], &nodes[i]);
-
-    vector<int> pre;
-    BST_PreOrder(&nodes[0], pre);
-
-    vector<int> pos;
-    BST_PostOrder(&nodes[0], pos);
-
-    answer.push_back(pre);
-    answer.push_back(pos);
-
+    for (int i=0; i<nodeinfo.size(); i++)
+    {
+        xtonum[nodeinfo[i][0]] = i+1;
+    }
+    // y가 큰 장소가 먼저 삽입되어야
+    // x만 가지고 BST만들 수 있음.
+    sort(nodeinfo.begin(), nodeinfo.end(), cmp);
+    BST bst;
+    
+    for (int i=0; i<nodeinfo.size(); i++)
+    {
+        bst.insert(nodeinfo[i][0], xtonum[nodeinfo[i][0]]);
+    }
+    
+    bst.PreOrder(answer[0]);
+    bst.PostOrder(answer[1]);
+    
     return answer;
 }
