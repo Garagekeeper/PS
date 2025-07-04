ff5e0b24452ca32a0fe3c6004aee4b0c27646c8a
Supply_depot <hj06869@naver.com>
2025-07-04 21:35:22 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 using namespace std;
 
 int solution(int n, int a, int b)
@@ -7,9 +5,10 @@ int solution(int n, int a, int b)
     
     int answer = 0;
     
-    while(a != b) {
-        a = (a / 2) + (a % 2);
-        b = (b / 2) + (b % 2);
+    while(a != b) 
+    {
+        a = a / 2 + a % 2;
+        b = b / 2 + b % 2;
         answer++;
     }
     return answer;
