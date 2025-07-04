0b092d202a2a5d57db47b62c453caaf986fca175
Supply_depot <hj06869@naver.com>
2025-07-05 06:55:30 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 51.19 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -4,31 +4,59 @@
 #include <unordered_map>
 using namespace std;
 
-unordered_map<string, int> sellmoney; // 멤버 별 판매 금액
-unordered_map<string, string> ref; // 멤버 별 추천인
+unordered_map<string, string> refMap;
+unordered_map<string, int> profitMap;
 
-void func(string name, int money) {
-    if(name == "-") return; // "-" 추천인이 없을 경우 종료
-    int tmp = money * 0.1; // 추천인에게 들어갈 판매 금액의 10%
-    sellmoney[name] += (money - tmp); // 판매 금액의 90%만 수익으로 더해줌
-    if(money >= 1){ // 추천인에게 줄 판매 금액의 10%가 1 이상일 때만 수행
-        func(ref[name], tmp);
-    }
-    return;
+void setProfit(string member, int amount)
+{
+    int total = amount * 100;
+    int distribution;
+    
+    do
+    {
+        if (total <= 9)
+        {
+            profitMap[member]+=total;
+            break;
+        }
+        else 
+        {
+            distribution = total / 10;
+            profitMap[member]+= total - distribution;
+            
+            member = refMap[member];
+            total = distribution;
+        }
+    }while(member != "Center");
+    
 }
 
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
     vector<int> ans;
-    for(int i=0; i<enroll.size(); i++){
-        ref[enroll[i]] = referral[i]; // 추천인 정보 저장
+    
+    
+    // refMap 채우기
+    for (int i=0; i<enroll.size(); ++i)
+    {
+        string ref;
+        if (referral[i] == "-")
+            ref="Center";
+        else
+            ref = referral[i];
+        
+        refMap[enroll[i]]= ref;
     }
     
-    for(int i=0; i<seller.size(); i++){
-        func(seller[i], amount[i] * 100); // 판매자들의 판매 금액과 추천인에게 들어갈 금액 업데이트
+    // 판매금액을 통한 이익금 처리
+    for (int i = 0; i < amount.size(); i++)
+    {
+        setProfit(seller[i], amount[i]);
     }
     
-    for(int i=0; i<enroll.size(); i++) {
-        ans.push_back(sellmoney[enroll[i]]);
+    for (auto& e : enroll)
+    {
+        ans.push_back(profitMap[e]);
     }
+    
     return ans;
 }
