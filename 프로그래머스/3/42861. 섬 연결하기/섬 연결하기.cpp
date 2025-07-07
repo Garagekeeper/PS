7246275ad8727ec1d1854192a81e51034977cc7c
Supply_depot <hj06869@naver.com>
2025-07-07 23:52:14 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> parents;
+vector<int> costsvec;
+
+int CalcCost(int x, int cost)
+{
+    if (parents[x] == x)
+        return cost;
+    else
+        return CalcCost(parents[x], cost + costsvec[x]);
+}
+
+int Find(int x)
+{
+    if (parents[x] == x)
+        return (x);
+    return Find(parents[x]);
+}
+
+void Union(int a, int b, int& answer, int& cost) 
+{
+    int A = Find(a);
+    int B = Find(b);
+    
+    if (A!=B)
+    {
+        if (A < B) 
+            parents[B] = A;
+        else 
+            parents[A] = B;
+        answer += cost;
+    }
+}
+
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;
+    parents = vector<int>(100);
+    costsvec = vector<int>(100,0);
+    
+    for (int i=0; i<n; i++)
+    {
+        parents[i] = i;
+    }
+    
+    sort(costs.begin(), costs.end() ,[](vector<int>& a, vector<int>& b){
+        return a[2] < b[2];
+    });
+    
+    for (int i = 0; i < costs.size(); ++i)
+    {
+        int a = costs[i][0];
+        int b = costs[i][1];
+        int cost = costs[i][2];
+        
+        Union(a,b,answer,cost);
+    }
+    
+    return answer;
+}
