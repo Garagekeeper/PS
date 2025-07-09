f99bb92afce88dbe5becf3ee5412fb86e1590a20
Supply_depot <hj06869@naver.com>
2025-07-10 04:46:46 +0900

    [level 2] Title: 튜플, Time: 14.98 ms, Memory: 5.44 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -1,15 +1,56 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
-#include <set>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    unordered_map<int, int> m;
+    vector<pair<int, int>> frqv;
+    
+    string temp="";
+    int maxNum = 0;
+    for (auto it = s.begin(); it != s.end(); ++it)
+    {
+        if ( '0' <= *it && *it <= '9')
+            temp += *it;
+        else
+        {
+            if (temp == "") continue;
+            maxNum = max(maxNum, stoi(temp));
+            m[stoi(temp)]++;
+            temp="";
+        }
+        
+    }
+    
+    frqv = vector<pair<int,int>> (m.begin(), m.end());
+    
+    sort(frqv.begin(), frqv.end(), [](auto& a, auto& b){
+        return a.second > b.second;
+    });
+    
+     for (auto& [val, cnt] : frqv)
+        answer.push_back(val);
+    
+    return answer;
+}
+
+/*
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
 vector<int> solution(string s) {
     vector<int> answer;
     vector<vector<int>> setVec;
-    set<int> answerSet;
+    unordered_set<int> answerSet;
     
     bool canInsert = false;
     int index = 0;
@@ -59,3 +100,4 @@ vector<int> solution(string s) {
     
     return answer;
 }
+*/
