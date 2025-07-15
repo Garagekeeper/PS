c4d6fc7a8080e5e10f0f4661aea34f2a01447efb
Supply_depot <hj06869@naver.com>
2025-07-16 01:13:51 +0900

    [level 2] Title: 배달, Time: 0.35 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,66 +1,72 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <climits>
 #include <queue>
-
+#include <climits>
+#include <unordered_map>
+#include <iostream>
 using namespace std;
 
 vector<bool> visited;
-// 0: minDist, 1: node right before this
+//0: min Dist,  1: node right before this
 vector<vector<int>> dijkstraVec;
 unordered_map<int, vector<pair<int,int>>> adjList;
+int GN;
 
-void Dijkstra(int N, int start)
+void Dijkstra(int start)
 {
-    visited.resize(N+1, false);
-    dijkstraVec.resize(N+1, vector<int>(2, INT_MAX));
-    priority_queue<pair<int, int>> pq;
+    dijkstraVec.resize(GN, vector<int>(2, INT_MAX));
+    dijkstraVec[start][0] = 0;
+    dijkstraVec[start][1] = 1;
     
-    dijkstraVec[1][0] = 0;
-    dijkstraVec[1][1] = 1;
+    visited.resize(GN,false);
     
-    pq.push({1,start});
+    // dist, nodeNum
+    // 비용이 낮은게 위로 오도록
+    auto cmp = [](const pair<int,int>& a, pair<int,int>& b){
+        return a.first > b.first;
+    };
+    priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(cmp)> pq(cmp);
+    pq.push({0,start});
+    
     while(!pq.empty())
     {
-        int nodeNum = pq.top().first;
-        int currDist = pq.top().second;
-        pq.pop();
-
-        for (auto& [to, weight] : adjList[nodeNum])
+        auto [dist, currentNode] = pq.top(); pq.pop();
+        
+        if (visited[currentNode]) continue;
+        visited[currentNode] = true;
+        
+        for (auto& [v, weight] : adjList[currentNode])
         {
-            if (dijkstraVec[to][0] > dijkstraVec[nodeNum][0] + weight)
+            int startToCurrnetNode = dijkstraVec[currentNode][0];
+            int startToV = dijkstraVec[v][0];
+            int currnetNodeToV = weight;
+            
+            if (startToV > startToCurrnetNode + currnetNodeToV)
             {
-                dijkstraVec[to][0] = dijkstraVec[nodeNum][0] + weight;
-                dijkstraVec[to][1] = nodeNum;
-                pq.push({to, dijkstraVec[to][0]});
+                dijkstraVec[v][0] = startToCurrnetNode + currnetNodeToV;
+                dijkstraVec[v][1] = currentNode;
+                pq.push({dijkstraVec[v][0], v});
             }
         }
     }
-    
 }
 
-int solution(int N, vector<vector<int> > road, int K) {
+int solution(int N, vector<vector<int>> road, int K) {
     int answer = 0;
+    GN = N + 1;
     
-    for (auto& vec : road)
+    for (auto& edge : road)
     {
-        for (auto& [to, weight] : adjList[vec[0]])
-        {
-            if (to == vec[1]) weight = min (weight, vec[2]);
-            continue;
-        }
-        
-        adjList[vec[0]].push_back({vec[1], vec[2]});
-        adjList[vec[1]].push_back({vec[0], vec[2]});
+        adjList[edge[0]].push_back({edge[1], edge[2]});
+        adjList[edge[1]].push_back({edge[0], edge[2]});
     }
     
-    Dijkstra(N, 1);
-    
-    for (int i=1; i<N+1; i++)
+    Dijkstra(1);
+
+    for (int i = 1; i < GN; i++)
     {
         if (dijkstraVec[i][0] <= K) answer++;
     }
-
+    
     return answer;
 }
