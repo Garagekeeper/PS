b8d9e7763404204c7a87c3ecd0f856e5a1442ca6
Supply_depot <hj06869@naver.com>
2025-07-22 02:14:38 +0900

    [level 2] Title: N-Queen, Time: 235.25 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.cpp
+++ 프로그래머스/2/12952. N－Queen/N－Queen.cpp
@@ -1,38 +1,90 @@
-#include <iostream>
+#include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
-int answer = 0;
+vector<vector<bool>> board;
+vector<int> qpos;
 int N;
-vector<int> board; // i번째 행에 퀸이 놓인 열 번호
+int answer = 0;
 
-bool CanPlace(int row, int col) {
-    for (int i = 0; i < row; i++) {
-        // 같은 열 || 대각선(좌우 대각선 모두)
-        if (board[i] == col || abs(board[i] - col) == row - i)
-            return false;
+bool CellCheck(int x, int y, int cnt)
+{
+    if (cnt == 0) return true;
+    //cout << "x " << x << " y " << y << endl;
+    // 십자 체크
+    for (int i=0; i<N; i++)
+    {
+        if (board[i][y] == true) return false;
+        if (board[x][i] == true) return false;
     }
+    
+
+    int nx, ny;
+    // 왼쪽 대각선
+    // 30 41
+    // 20 31 42
+    // 10 21 32 43
+    // 00 11 22 33 44
+    // 01 12 23 34
+    // 02 13 24
+    // 03 14
+    
+    
+    nx = x;
+    ny = y;
+    
+    while (true)
+    {
+        nx = nx - 1;
+        ny = ny - 1;
+        if (nx < 0 || ny < 0) break;
+        if (board[nx][ny] == true) return false;
+    }
+    
+    // 오른쪽 대각선
+    nx = x;
+    ny = y;
+    
+    while (true)
+    {
+        nx = nx - 1;
+        ny = ny + 1;
+        if (nx < 0 || ny >= N) break;
+        if (board[nx][ny] == true) return false;
+    }
+    
     return true;
 }
 
-void dfs(int row) {
-    if (row == N) {
-        answer++;
-        return;
+int dfs(int row, int num)
+{
+    
+     if (num >= N)
+    {
+        return 1;
     }
-
-    for (int col = 0; col < N; col++) {
-        if (CanPlace(row, col)) {
-            board[row] = col;
-            dfs(row + 1);
+    
+    for (int j = 0; j<N; j++)
+        {
+            if (board[row][j] == false)
+            {
+                if (CellCheck(row,j, num))
+                {
+                    board[row][j] = true;
+                    answer += dfs(row+1, num + 1);
+                    board[row][j] = false;
+                }
+            }
         }
-    }
+    
+    return 0;
 }
 
 int solution(int n) {
     N = n;
-    board = vector<int>(N);
-    dfs(0);
+    board.resize(n, vector<bool>(n, false));
+    qpos.resize(n,0);
+    dfs(0,0);
     return answer;
 }
--- 프로그래머스/2/12952. N－Queen/README.md
+++ 프로그래머스/2/12952. N－Queen/README.md
@@ -1,10 +1,10 @@
 # [level 2] N-Queen - 12952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 116.74 ms
+메모리: 4.2 MB, 시간: 235.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 20일 22:50:48
+2025년 07월 22일 02:14:35
 
 ### 문제 설명
 
