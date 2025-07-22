e92fee03edac396a4232452c83b99bdef4455de9
Supply_depot <hj06869@naver.com>
2025-07-22 17:44:48 +0900

    [level 2] Title: 피로도, Time: 1.15 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,34 +1,34 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
 using namespace std;
-vector<bool> visited;
+
 vector<vector<int>> Gdungeons;
+vector<bool> visited;
 int answer = -1;
 int N;
-int dfs(int k, int depth)
+using namespace std;
+
+void dfs(int k, int depth)
 {
-    // 탐색의 깊이가 던전의 개수보다 많으면 종료
-    if (N == depth) return depth;
-
-
+    answer = max(answer, depth);
     for (int i=0; i<N; i++)
     {
-      if (visited[i] || k < Gdungeons[i][0]) continue;
-      visited[i] = true;
-      answer = max(answer, dfs(k - Gdungeons[i][1], depth + 1));
-      visited[i] = false;
+
+        if (visited[i]) continue;
+        if (k < Gdungeons[i][0]) continue;
+        
+        visited[i] = true;
+        dfs(k - Gdungeons[i][1], depth+1);
+        visited[i] = false;
     }
-    return depth;
+    
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    
     Gdungeons = dungeons;
     N = dungeons.size();
-    visited.resize(N, false);
-    dfs(k, 0);
+    visited.resize(N,false);
+    dfs(k,0);
     return answer;
-        
 }
