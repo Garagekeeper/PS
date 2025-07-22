8814203fde2203f180b5c4dcca20824d90b7431d
Supply_depot <hj06869@naver.com>
2025-07-22 21:07:14 +0900

    [level 2] Title: 양궁대회, Time: 29.62 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
@@ -1,47 +1,99 @@
+#include <string>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
-int maxDiff = 0;
+// 개수를 기반으로 dfs
+vector<int> Ainfo;
+vector<int> Rinfo;
+int N;
+int maxScore=-987654321;
 vector<int> answer;
-
-void dfs(int idx, int arrowsLeft, vector<int>& ryan, const vector<int>& appeach) {
-    if (idx == 11) {
-        if (arrowsLeft > 0) ryan[10] += arrowsLeft; // 남은 화살 마지막에 몰아주기
-
-        int ryanScore = 0, appeachScore = 0;
-        for (int i = 0; i < 11; ++i) {
-            if (ryan[i] > appeach[i]) ryanScore += 10 - i;
-            else if (appeach[i] > 0) appeachScore += 10 - i;
+vector<int> visited;
+int CalcScore()
+{
+    int Ascore = 0;
+    int Rscore = 0;
+    for (int i=10; i>=1; i--)
+    {
+        if (Ainfo[i]==Rinfo[i] && Ainfo[i] == 0) continue;
+        if (Ainfo[i]>=Rinfo[i])
+        {
+            Ascore += i;
         }
-
-        if (ryanScore > appeachScore) {
-            int diff = ryanScore - appeachScore;
-            if (diff > maxDiff || 
-                (diff == maxDiff && lexicographical_compare(answer.rbegin(), answer.rend(), ryan.rbegin(), ryan.rend()))) {
-                maxDiff = diff;
-                answer = ryan;
-            }
+        else 
+        {
+            Rscore += i;
         }
-
-        if (arrowsLeft > 0) ryan[10] -= arrowsLeft; // 원상복구
-        return;
     }
+    
+    return Rscore - Ascore;
+}
 
-    // 화살 쏘기
-    if (arrowsLeft > appeach[idx]) {
-        ryan[idx] = appeach[idx] + 1;
-        dfs(idx + 1, arrowsLeft - ryan[idx], ryan, appeach);
-        ryan[idx] = 0; // 백트래킹
+bool dfs(int cnt)
+{
+    // cnt가 n보다 크거나 같으면 종료
+    // 총 점수를 계산해서 같거나 더 높으면 갱신
+    if (cnt > N) return false;
+    if (cnt == N)
+    {
+        int res = CalcScore();
+        if (res <= 0 ) return false;
+        if (res >= maxScore) 
+        {
+            maxScore = res;
+            answer = Rinfo;
+        }
+        return false;
     }
-
-    // 안 쏘기
-    dfs(idx + 1, arrowsLeft, ryan, appeach);
+    
+    // 10부터 선택하도록
+    for (int i=10; i>=0; i--)
+    {
+        // 이미 라이언이 점수를 가져온 경우
+        // 더 맞혀봐야 점수는 i점
+        //if (visited[i] = true)
+        if (Ainfo[i] < Rinfo[i]) continue;
+        if (N - cnt < Ainfo[i] && i != 0) continue;
+        
+        if (i == 0)
+        {
+            Rinfo[i]+=N - cnt;
+            bool flag = dfs(cnt+N - cnt);
+            Rinfo[i]-=N - cnt;
+        }
+        else 
+        {
+            Rinfo[i]+=Ainfo[i] + 1;
+            bool flag = dfs(cnt+Ainfo[i] + 1);
+            Rinfo[i]-=Ainfo[i] + 1;
+        }
+        
+        
+        //if (!flag) visited[i] = true;
+        
+    }
+    return true;
 }
 
 vector<int> solution(int n, vector<int> info) {
-    answer = {-1};
-    vector<int> ryan(11, 0);
-    dfs(0, n, ryan, info);
-    return answer;
+    Ainfo = vector<int>(info.rbegin(), info.rend());
+    N = n;
+    answer.resize(11, 0);
+    Rinfo.resize(11, 0);
+    visited.resize(11,0);
+    dfs(0);
+    vector<int> ansvec;
+    bool flag = false;
+    for (auto& e : answer)
+    {
+        if (e != 0) flag = true;
+    }
+    
+    if (flag)
+    {
+        return ansvec = vector<int> (answer.rbegin(), answer.rend());
+    }
+    
+    return {-1};
 }
