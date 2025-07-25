bb6f9fd1b661dc17b9a33af1bafff41227b13f53
Supply_depot <hj06869@naver.com>
2025-07-25 16:25:13 +0900

    [level 3] Title: 사라지는 발판, Time: 5.46 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.cpp
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.cpp
@@ -1,53 +1,66 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 using namespace std;
 
-int dx[4] = {0,0,-1,1};
-int dy[4] = {-1,1,0,0};
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
 
-int n, m;
+vector<int> Apos;
+vector<int> Bpos;
+vector<vector<int>> gboard;
 
-bool OOB(int x, int y){
-    return x<0 || x>=n || y<0 || y>=m;
+char turn = 'A';
+int answer = 0;
+
+bool CanGo(int x, int y)
+{
+    if (x < 0 || x >= gboard.size()) return false;
+    if (y < 0 || y >= gboard[0].size()) return false;
+    if (gboard[x][y] == 0) return false;
+    return true;
 }
 
-bool vis[5][5]; // 방문 여부(0일 경우 해당 칸이 아직 남아있음을 의미)
-vector<vector<int>> block;
-
-// 현재 상태에서 둘 다 최적의 플레이를 할 때 남은 이동 횟수
-// 반환 값이 짝수 : 플레이어가 패배함을 의미, 홀수 : 플레이어가 승리함을 의미
-// curx, cury : 현재 플레이어의 좌표, opx, opy : 상대 플레이어의 좌표
-int solve(int curx, int cury, int opx, int opy){
-    // 플레이어가 밟고 있는 발판이 사라졌다면
-    if(vis[curx][cury]) return 0;    
-    int ret = 0;
-    // 플레이어를 네 방향으로 이동시켜 다음 단계로 진행할 예정
-    for(int dir = 0; dir < 4; dir++){
-        int nx = curx + dx[dir];
-        int ny = cury + dy[dir];
-        if(OOB(nx,ny) || vis[nx][ny] || !block[nx][ny]) continue;
-        vis[curx][cury] = 1; // 플레이어가 직전에 있던 곳에 방문 표시를 남김
+int dfs(char turn)
+{
+    int count = 0;
+    vector<int>& player = turn == 'A' ? Apos : Bpos;
+    int cx = player[0];
+    int cy = player[1];
+    if (gboard[cx][cy] == 0) return 0;
+       
+    for (int i=0; i<4; i++)
+    {
+        int nx = cx + dx[i];
+        int ny = cy + dy[i];
+        if (!CanGo(nx, ny)) continue;
         
-        // 플레이어를 dir 방향으로 이동시켰을 때 턴의 수
-        // 다음 함수를 호출할 때 opx, opy, nx, ny 순으로 호출해야 함에 주의
-        int val = solve(opx, opy, nx, ny)+1;      
-        
-        // 방문 표시 해제
-        vis[curx][cury] = 0;        
-        
-        // 1. 현재 저장된 턴은 패배인데 새로 계산된 턴은 승리인 경우
-        if(ret % 2 == 0 && val % 2 == 1) ret = val; // 바로 갱신
-        // 2. 현재 저장된 턴과 새로 계산된 턴이 모두 패배인 경우
-        else if(ret % 2 == 0 && val % 2 == 0) ret = max(ret, val); // 최대한 늦게 지는걸 선택
-        // 3. 현재 저장된 턴과 새로 계산된 턴이 모두 승리인 경우
-        else if(ret % 2 == 1 && val % 2 == 1) ret = min(ret, val); // 최대한 빨리 이기는걸 선택
-        
+        player[0] = nx;
+        player[1] = ny;
+        gboard[cx][cy] = 0;
+        int res = dfs('A' + 'B' - turn) + 1;
+        gboard[cx][cy] = 1;
+        player[0] = cx;
+        player[1] = cy;
+            
+        // A부터 시작.
+        // 턴이 2로 나뉘어떨어지면 A가 패배
+        // 현재까지 지는 경우였는데 새 경로가 이기는 경로인 경우
+        if (count % 2 == 0 && res % 2 == 1) count = res;
+        // 현재까지 지는 경우, 새 경로도 지는 경우. (최대한 늦게 지기)
+        else if (count % 2 == 0 && res % 2 == 0) count = max (count, res);
+        // 현재까지 이기는 경우, 새 경로도 이기는 경우. (최대한 빨리 이기기)
+        else if (count % 2 == 1 && res % 2 == 1) count = min (count, res);
     }
-    return ret;
+    return count;
 }
 
-int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
-    n = board.size();
-    m = board[0].size();
-    block = board;
-    return solve(aloc[0], aloc[1], bloc[0], bloc[1]);
+int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) 
+{
+    gboard = board;
+    Apos = aloc;
+    Bpos = bloc;
+    
+    answer = dfs('A');
+    
+    return answer;
 }
