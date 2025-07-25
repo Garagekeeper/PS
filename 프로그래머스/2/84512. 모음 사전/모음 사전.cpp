7d8828369709c1bbe32dff71b0146daeb3563308
Supply_depot <hj06869@naver.com>
2025-07-25 16:04:33 +0900

    [level 2] Title: 모음 사전, Time: 0.47 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int answer = 0;
+int cnt = 0;
+string charList = "AEIOU";
+
+void dfs(string current, string target)
+{
+    if (answer != 0) return;
+    if (current.size() > 5) return;
+    
+    if (current == target)
+    {
+        answer = cnt;
+        return;
+    }
+        
+    cnt++;
+    for (int i=0; i<5; i++)
+    {
+        dfs(current + charList[i], target);
+        if (answer != 0) return;
+    }
+}
+
+int solution(string word) {
+    dfs("", word);
+    return answer;
+}
