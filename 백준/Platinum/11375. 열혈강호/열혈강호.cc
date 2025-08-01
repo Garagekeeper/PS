69aa99193b0ddb59259ed5e5aa7eb19fffa4cd3d
Supply_depot <hj06869@naver.com>
2025-08-01 19:28:03 +0900

    [Platinum IV] Title: 열혈강호, Time: 568 ms, Memory: 5992 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <memory.h>
+
+using namespace std;
+
+int N, M;
+vector<int> matchedEmployee;
+vector<bool> jobVisited;
+unordered_map<int, vector<int>> jobGraph;
+int answer = 0;
+
+bool dfs(int employee)
+{
+    for (int job : jobGraph[employee])
+    {
+        if (jobVisited[job]) continue;
+        jobVisited[job] = true;
+
+        // 현재 작업에 매칭된 사람이 없거나
+        // 기존에 매칭된 사람이 다른 직업에 매칭 가능하면
+        // 매칭
+        if (matchedEmployee[job] == 0 || dfs(matchedEmployee[job]))
+        {
+            matchedEmployee[job] = employee;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    //ios::sync_with_stdio(false);
+    //cin.tie(NULL);
+    cin >> N >> M;
+
+    matchedEmployee.resize(M+1);
+    jobVisited.resize(M+1);
+
+    for (int i=1; i<=N; i++)
+    {
+        int cnt;
+        cin >> cnt;
+        for (int j=0; j<cnt; j++)
+        {
+            int job;
+            cin >> job;
+            jobGraph[i].push_back(job);
+        }
+    }
+
+   
+
+    for (int i=1; i<=N; i++)
+    {
+        fill(jobVisited.begin(), jobVisited.end(), false);
+        answer +=  dfs(i) ? 1 : 0;
+    }
+    cout << answer <<'\n';
+    return 0;
+}
