627e11690d72fcbafdee5d99e1b1679c74639d2d
Supply_depot <hj06869@naver.com>
2025-08-04 16:50:59 +0900

    [level 2] Title: 양궁대회, Time: 27.97 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
@@ -1,24 +1,23 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 // 개수를 기반으로 dfs
 vector<int> Ainfo;
 vector<int> Rinfo;
 int N;
-int maxScore=-987654321;
+int maxScore=-1;
 vector<int> answer;
-vector<int> visited;
 int CalcScore()
 {
     int Ascore = 0;
     int Rscore = 0;
     for (int i=10; i>=1; i--)
     {
-        if (Ainfo[i]==Rinfo[i] && Ainfo[i] == 0) continue;
         if (Ainfo[i]>=Rinfo[i])
         {
+            if (Ainfo[i] == 0) continue;
             Ascore += i;
         }
         else 
@@ -30,21 +29,25 @@ int CalcScore()
     return Rscore - Ascore;
 }
 
-bool dfs(int cnt)
+void dfs(int cnt)
 {
     // cnt가 n보다 크거나 같으면 종료
     // 총 점수를 계산해서 같거나 더 높으면 갱신
-    if (cnt > N) return false;
+    if (cnt > N) return;
     if (cnt == N)
     {
         int res = CalcScore();
-        if (res <= 0 ) return false;
+        // 동점이거나 진 경우
+        if (res <= 0 ) return;
+        // 이겼는데 최고점수보다 같거나 큰 경우
+        // 큰 숫자부터 선택하기 때문에 같은 점수의 
+        // 더 낮은 조합을 따지기 위해서 등호가 포함되어야
         if (res >= maxScore) 
         {
             maxScore = res;
             answer = Rinfo;
         }
-        return false;
+        return;
     }
     
     // 10부터 선택하도록
@@ -52,28 +55,23 @@ bool dfs(int cnt)
     {
         // 이미 라이언이 점수를 가져온 경우
         // 더 맞혀봐야 점수는 i점
-        //if (visited[i] = true)
         if (Ainfo[i] < Rinfo[i]) continue;
-        if (N - cnt < Ainfo[i] && i != 0) continue;
         
         if (i == 0)
         {
+            // 0점인 경우 남은거 다 박기
             Rinfo[i]+=N - cnt;
-            bool flag = dfs(cnt+N - cnt);
+            dfs(cnt+N - cnt);
             Rinfo[i]-=N - cnt;
         }
         else 
         {
+            // i점을 얻기위한 최소한의 화살 개수를 더해서 dfs진행
             Rinfo[i]+=Ainfo[i] + 1;
-            bool flag = dfs(cnt+Ainfo[i] + 1);
+            dfs(cnt+Ainfo[i] + 1);
             Rinfo[i]-=Ainfo[i] + 1;
         }
-        
-        
-        //if (!flag) visited[i] = true;
-        
     }
-    return true;
 }
 
 vector<int> solution(int n, vector<int> info) {
@@ -81,18 +79,12 @@ vector<int> solution(int n, vector<int> info) {
     N = n;
     answer.resize(11, 0);
     Rinfo.resize(11, 0);
-    visited.resize(11,0);
     dfs(0);
-    vector<int> ansvec;
     bool flag = false;
-    for (auto& e : answer)
-    {
-        if (e != 0) flag = true;
-    }
     
-    if (flag)
+    if (any_of(answer.begin(), answer.end(), [](int x){ return x != 0; }))
     {
-        return ansvec = vector<int> (answer.rbegin(), answer.rend());
+        return vector<int>(answer.rbegin(), answer.rend());
     }
     
     return {-1};
