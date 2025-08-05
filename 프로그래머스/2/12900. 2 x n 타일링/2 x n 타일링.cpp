ac903c174649b63c9a44b42e730758aa72ad077d
Supply_depot <hj06869@naver.com>
2025-08-05 22:25:07 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.30 ms, Memory: 3.93 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.cpp
@@ -2,18 +2,20 @@
 #include <vector>
 
 using namespace std;
+
 const int MOD = 1'000'000'007;
+vector<vector<int>> cahche;
 
-
-int solution(int n) {
-    
-    vector<int> dp(n+1,0);
+int solution(int n) 
+{
+    int answer = 0;
+    vector<int> dp(n+1);
     dp[1] = 1;
     dp[2] = 2;
     
-    for (int i = 3; i <= n; i++)
+    for (int i=3; i<=n; i++)
     {
-        dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+        dp[i] = ( dp[i-1] + dp[i-2] ) % MOD;
     }
     
     return dp[n];
--- 프로그래머스/2/12900. 2 x n 타일링/README.md
+++ 프로그래머스/2/12900. 2 x n 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 2] 2 x n 타일링 - 12900 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12900) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12900?language=cpp) 
 
 ### 성능 요약
 
-메모리: 3.85 MB, 시간: 0.25 ms
+메모리: 3.93 MB, 시간: 0.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 03일 23:30:49
+2025년 08월 05일 22:25:05
 
 ### 문제 설명
 
