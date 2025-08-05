0432dcf90037af8269567a1391fa2b32b2ef7314
Supply_depot <hj06869@naver.com>
2025-08-05 19:17:38 +0900

    [level 2] Title: 피보나치 수, Time: 3.32 ms, Memory: 6.69 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=cpp#) 
 
 ### 성능 요약
 
-메모리: 6.63 MB, 시간: 3.15 ms
+메모리: 6.69 MB, 시간: 3.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 05일 18:17:35
+2025년 08월 05일 19:17:36
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -1,9 +1,11 @@
 #include <string>
 #include <vector>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
+
 #define MOD 1234567;
-//const int MOD = 1234567
 vector<int> cache;
 
 int Fibo(int n)
@@ -13,10 +15,28 @@ int Fibo(int n)
     return cache[n] = (Fibo(n-1) + Fibo(n-2)) % MOD;
 }
 
+// 정확도 문제로 큰 n에서는 사용 불가
+// 이 환경에서는 71부터 오차 발생
+// 변형해서 써야함
+int Fibo2(int n)
+{
+    if (n <= 70)
+    {
+        long double temp = pow((1 + sqrt(5))/2, n);
+        temp -= pow((1 - sqrt(5))/2, n);
+        temp /= sqrt(5);
+        int result = static_cast<long long>(round(temp)) % MOD;
+        return result;
+    }
+    else 
+    {
+        
+    }
+
+}
+
 int solution(int n) 
 {
-    int answer = 0;
     cache.resize(n+1);
-    answer = Fibo(n);
-    return answer;
+    return Fibo(n);
 }
