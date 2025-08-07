71497c42264020b2b35b861c73f71ce54b6ca223
Supply_depot <hj06869@naver.com>
2025-08-07 17:22:44 +0900

    [level 3] Title: 등굣길, Time: 0.08 ms, Memory: 3.87 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#) 
 
 ### 성능 요약
 
-메모리: 3.81 MB, 시간: 0.08 ms
+메모리: 3.87 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 07일 11:49:11
+2025년 08월 07일 17:22:42
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -8,34 +8,34 @@ using namespace std;
 int solution(int m, int n, vector<vector<int>> puddles) 
 {
     int answer = 0;
-    vector<vector<int>> land(n+1,vector<int>(m+1, 1));
-    vector<vector<int>> dp(n+1,vector<int>(m+1, 0));
+    vector<vector<int>> land(m+1,vector<int>(n+1, 1));
+    vector<vector<int>> dp(m+1,vector<int>(n+1, 0));
     
     for (int i=0; i < puddles.size(); i++)
     {
         if (puddles[i].size() == 0) continue;
         int x = puddles[i][0];
         int y = puddles[i][1];
-        land[y][x] = 0;
+        land[x][y] = 0;
     }
     
     
-    for (int i=1; i<=n; i++)
+    for (int i=1; i<=m; i++)
     {
         if (land[i][1] == 0) break;
         dp[i][1] = 1;
     }
     
-    for (int i=1; i<=m; i++)
+    for (int i=1; i<=n; i++)
     {
         if (land[1][i] == 0) break;
         dp[1][i] = 1;
     }
     
     
-    for (int i=2; i<=n;i++)
+    for (int i=2; i<=m;i++)
     {
-        for (int j=2; j<=m; j++)
+        for (int j=2; j<=n; j++)
         {
             if (land[i][j] != 0)
             {
@@ -43,19 +43,6 @@ int solution(int m, int n, vector<vector<int>> puddles)
             }
         }
     }
-    /*
-    for (int i=1; i<=n;i++)
-    {
-        for (int j=1; j<=m; j++)
-        {
-            {
-                cout << "[" <<dp[i][j] << "]";
-            }
-        }
-        cout << endl;
-    }
-    */
-
     
-    return dp[n][m];
+    return dp[m][n];
 }
