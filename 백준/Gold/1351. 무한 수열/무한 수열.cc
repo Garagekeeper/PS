26416c1b8e0e42ac7b7cbd1fd28039318bec25ee
Supply_depot <hj06869@naver.com>
2025-08-09 09:54:55 +0900

    [Gold V] Title: 무한 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.cc
+++ 백준/Gold/1351. 무한 수열/무한 수열.cc
@@ -11,8 +11,6 @@ unordered_map<long long, long long> cache;
 
 long long dfs(long long num)
 {
-    if (num==0) return 1;
-    if (num==1) return 2;
     if (cache.find(num) != cache.end()) return cache[num];
     return cache[num] = dfs(num/P) + dfs(num/Q);
 }
@@ -20,7 +18,9 @@ long long dfs(long long num)
 int main()
 {
     cin >> N >> P >> Q;
-
+    
+    cache[0] = 1;
+    cache[1] = 2;
     
     long long res = dfs(N);
     cout << res << '\n';
