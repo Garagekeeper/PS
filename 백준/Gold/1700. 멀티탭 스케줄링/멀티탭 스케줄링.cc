57fe4cc88a6683161b031661d2848ecbf1c77d1e
Supply_depot <hj06869@naver.com>
2025-08-12 05:27:35 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
 #include <unordered_set>
+#include <unordered_map>
 #include <queue>
-#include <deque>
 using namespace std;
 
 int N, K;
-unordered_map<int, int> cntMap;
-unordered_map<int, deque<int>> timeMap;
+unordered_map<int, queue<int>> timeMap;
 struct Cmp
 {
     bool operator()(const int& a, const int& b)
     {
+        // a가 더이상 나오지 않는 경우 a를 먼저
         if (timeMap[a].size() == 0 && timeMap[b].size() != 0) return false;
+        // b가 더이상 나오지 않는 경우 b를 먼저
         if (timeMap[a].size() != 0 && timeMap[b].size() == 0) return true;
+        // 둘다 나오지 않는 경우 상관없음
         if (timeMap[a].size() == 0 && timeMap[b].size() == 0) return false;
+        // 둘 중 더 늦게 나오는데 앞으로
+        // true면 b가 우선
         return (timeMap[a].front() < timeMap[b].front());
     }
 };
@@ -25,36 +28,39 @@ struct Cmp
 int main()
 {
     cin >> N >> K;
+    
     vector<int> cntVec (K+1);
     vector<int> seq;
-
     unordered_set<int> plugged;
-    priority_queue<int, vector<int> , Cmp> pq;
-    vector<vector<int>> devicetime(K+1);
+    
     for (int i=1; i<=K; i++)
     {
         int name;
         cin >> name;
-        timeMap[name].push_back(i-1);
+        timeMap[name].push(i-1);
         seq.push_back(name);
     }
-
 
     int cnt = 0;
     for (int i=0; i<seq.size(); i++)
     {
-        timeMap[seq[i]].pop_front();
+        // 현재 시점 삭제
+        timeMap[seq[i]].pop();
 
+        // 이미 있는거면 넘어감
         if (plugged.find(seq[i]) != plugged.end()) continue;
 
+        // 꽉 찬 경우
         if (plugged.size() == N)
         {
-            pq = priority_queue<int, vector<int> , Cmp>(plugged.begin(), plugged.end());
+            // 지금 이후로 가장 늦게 등장하는 걸 뽑음
+            priority_queue<int, vector<int> , Cmp>pq (plugged.begin(), plugged.end());
             int top = pq.top();
             plugged.erase(top);
             cnt++;
         }
 
+        // seq[i] 기기 플러그 꼽기
         plugged.insert(seq[i]);
         
     }
