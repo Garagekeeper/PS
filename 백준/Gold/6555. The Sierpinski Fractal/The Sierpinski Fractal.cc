936ec93e8bf3368dd6fd1add01d4c5e8aff6d5ec
Supply_depot <hj06869@naver.com>
2025-10-07 19:32:19 +0900

    [Gold V] Title: The Sierpinski Fractal, Time: 16 ms, Memory: 4008 KB -BaekjoonHub

--- 백준/Gold/6555. The Sierpinski Fractal/The Sierpinski Fractal.cc
+++ 백준/Gold/6555. The Sierpinski Fractal/The Sierpinski Fractal.cc
@@ -1,31 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <set>
-#include <unordered_set>
-#include <unordered_map>
-#include <queue>
 using namespace std;
 
 int N;
 
-void DrawSierpinskiFractal(vector<string>& canvas, int n, int x, int y)
+void DrawSierpinskiFractal(vector<string>& canvas, int height, int x, int y)
 {
-    if (n == 2)
+    if (height==2)
     {
-        canvas[y][x] = '/';
-        canvas[y][x+1] = '\\';
-        canvas[y+1][x-1] = '/';
+        canvas[y][x-1] = '/';
+        canvas[y][x] = '\\';
+        canvas[y+1][x-2] = '/';
+        canvas[y+1][x-1] = '_';
         canvas[y+1][x] = '_';
-        canvas[y+1][x+1] = '_';
-        canvas[y+1][x+2] = '\\';
+        canvas[y+1][x+1] = '\\';
         return;
     }
-    int half = n / 2;
-    DrawSierpinskiFractal(canvas, half, x, y);
-    DrawSierpinskiFractal(canvas, half, x - half, y + half);
-    DrawSierpinskiFractal(canvas, half, x + half, y + half);
 
+    int half = height / 2;
+    DrawSierpinskiFractal(canvas, half, x , y);
+    DrawSierpinskiFractal(canvas, half, x - half , y + half);
+    DrawSierpinskiFractal(canvas, half, x + half , y + half);
 }
 
 
@@ -39,24 +34,21 @@ int main()
         cin >> N;
 
         if (N==0) break;
-
         
         int height = 1 << N;
         int width = 1 << (N+1);
         vector<string> canvas(height, string(width, ' '));
-        DrawSierpinskiFractal(canvas, height, height -1, 0);
 
-       for (int i = 0; i < height; ++i)
-       {
-        int end = width - 1;
-        while(end >= 0 && canvas[i][end] == ' ') end--;
-        if (end >= 0)
-            cout << canvas[i].substr(0,end+1);
+        DrawSierpinskiFractal(canvas, height, height, 0);
+        for (int i=0; i<height; i++)
+        {
+            int end = width - 1;
+            while(canvas[i][end] == ' ') end--;
+            cout << canvas[i].substr(0, end+1) << "\n";
+        }
         cout << "\n";
-       }
-       cout << "\n";
     }
-   
+
     return 0;
 }
 
