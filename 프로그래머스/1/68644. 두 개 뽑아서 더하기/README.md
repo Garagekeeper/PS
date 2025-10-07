27d27fc4751c2f8f339bfdb7474d0788a58788a6
Supply_depot <hj06869@naver.com>
2025-10-07 21:53:58 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.61 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=cpp#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=csharp) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.24 ms
+메모리: 31.3 MB, 시간: 4.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 23일 18:28:40
+2025년 10월 07일 21:53:56
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[] {};
+        List<int> list = new List<int>();
+        for (int i=0; i<numbers.Length; i++)
+        {
+            for (int j=i+1; j<numbers.Length; j++)
+            {
+                list.Add(numbers[i] + numbers[j]);
+            }
+        }
+        
+
+        answer = list.Distinct().OrderBy(x => x).ToArray();
+        return answer;
+    }
+}
