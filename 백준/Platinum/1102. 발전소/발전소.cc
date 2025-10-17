6a1811e88b82301161d23bd4106bd1dd60fd7ae0
Supply_depot <hj06869@naver.com>
2025-10-18 03:15:38 +0900

    [Platinum V] Title: 발전소, Time: 24 ms, Memory: 2280 KB -BaekjoonHub

---
+++ 백준/Platinum/1102. 발전소/발전소.cc
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+
+const int INF = 987654321;
+int N, P;
+
+int graph[16][16];
+int minimum = INF;
+int cost[1 << 16];
+
+int dfs(int mask)
+{
+    if (cost[mask] != -1)
+    {
+        return cost[mask];
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < N; ++i) 
+    {
+        if ((mask >> i) & 1) 
+            cnt++;
+    }
+
+    if (cnt >= P)
+    {
+        return 0;
+    }
+
+    cost[mask] = INF;
+    for (int i=0; i<N; i++)
+    {
+        if (!(mask & (1 << i))) continue;
+        for (int j=0; j<N; j++)
+        {
+            if (mask & (1 << j)) continue;
+            cost[mask] = min(dfs(mask), dfs(mask | ( 1 << j )) + graph[i][j]);
+        }
+    }
+
+    return cost[mask];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(NULL);
+    memset(cost, -1, sizeof(cost));
+    cin >> N;
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<N; j++)
+        {
+            int weight;
+            cin >> weight;
+            graph[i][j] = weight;
+        }
+    }
+
+    string onOffstat;
+    cin >> onOffstat;
+    int mask = 0;
+    for (int i=0; i<onOffstat.size(); i++)
+    {
+        if (onOffstat[i] == 'Y')
+        {
+            mask |= 1 << i;
+        }
+    }
+
+    cin >> P;
+
+    minimum = dfs(mask);
+
+    if (minimum == INF)
+    {
+        minimum = -1;
+    }
+
+    cout << minimum;
+
+    return 0;
+}
